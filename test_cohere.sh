c1d846e22c30b5e3bdf22b13edc213c3a1058289
Evans Mosomi <junior@Evanss-Laptop.local>
2025-04-23 15:07:25 -0400

    script for master registration

--- test_cohere.sh
+++ test_cohere.sh
@@ -1,17 +1,19 @@
 HOST="https://hack0425.north.internal.cohere.com/api"
 
-COUT=$(curl -X POST "${HOST}/v1/signin" -H "Content-Type: application/json" -d 'f"email": "evans@canyouhack0425.me", "password":"Evans123"}')
+# TODO: This login strategy will change when SSO is fully set up
+# As of now, the TEST_PASSWORD is "1234"*2
+COUT=$(curl -X POST "${HOST}/v1/signin" -H "Content-Type: application/json" -d '{"email": "evans@canyouhack0425.me", "password": "Evans123"}')
 
 echo "Setting NORTH JWT using: $COUT"
-#NORTH_TOKEN=$(echo #COUT | ja -r' token')
-NORTH_TOKEN=$(echo "$COUT" | grep '"token":"' | sed -E 's/.*"token":"([^"]*" •*/\1/')
+#NORTH_TOKEN=$(echo $COUT | jq -r '.token')
+NORTH_TOKEN=$(echo "$COUT" | grep '"token":"' | sed -E 's/.*"token":"([^"]*)".*/\1/')
 
-export MCP_SERVER_URL= "http://cohere-hackathon-production.up.railway.app"
+export MCP_SERVER_URL="https://cohere-hackathon-production.up.railway.app"
 
 curl --location "${HOST}/internal/v1/mcp_servers" \
 --header "Content-Type: application/json" \
 --header "Authorization: Bearer ${NORTH_TOKEN}" \
 --data '{
     "url": '"\"${MCP_SERVER_URL}\""',
-    "name": "RavenPack"
+    "name": "Bigdata_mcp_1"
 }'
